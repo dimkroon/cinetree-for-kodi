b86155cfb12de25c0600c26aafc76cf851835942
Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>
2025-01-08 16:43:43 +0100

    fixed typos, updated comments and other minor changes.
    Signed-off-by: Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>

--- plugin.video.cinetree/resources/lib/main.py
+++ plugin.video.cinetree/resources/lib/main.py
@@ -1,6 +1,6 @@
 
 # ------------------------------------------------------------------------------
-#  Copyright (c) 2022-2024 Dimitri Kroon.
+#  Copyright (c) 2022-2025 Dimitri Kroon.
 #  This file is part of plugin.video.cinetree.
 #  SPDX-License-Identifier: GPL-2.0-or-later.
 #  See LICENSE.txt
@@ -74,7 +74,7 @@ def list_my_films(addon, subcategory=None):
         films_list = ct_api.get_watched_films(subcategory == 'finished')
 
     if not films_list:
-        yield False
+        # yield False
         return
 
     films = (ct_data.create_film_item(film) for film in films_list)
@@ -257,7 +257,7 @@ def play_ct_video(stream_info: dict, title: str = ''):
             logger.debug("User canceled resume play dialog")
             return False
         elif result == 0:
-            play_item.setInfo('video', {'playcount': 1})
+            play_item.setInfo('video', {'playcount': '1'})
             play_item.setProperties({
                 'ResumeTime': str(resume_time),
                 'TotalTime': str(duration)
